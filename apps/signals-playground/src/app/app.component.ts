eaa510eb92aca304cf96eea0abb3f5fd3bb5e11c
drmoreira <drmoreira@outlook.pt>
2025-04-07 16:10:50 +0100

    adds sginals playground application

---
+++ apps/signals-playground/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <div class="component">
+      <h1>Siuuugnals!</h1>
+      <hr />
+
+      <h1>Counter: {{ counter }}</h1>
+      <hr />
+
+      <button (click)="handleClick()">Click me!</button>
+    </div>
+  `
+})
+export class AppComponent {
+  counter = 0;
+
+  handleClick() {
+    this.counter = this.counter + 1;
+  }
+}
